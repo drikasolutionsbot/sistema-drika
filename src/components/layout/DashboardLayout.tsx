2a4a3e399f3164f1e18cad198bd680ea125226a3
PCONCONSTRUNET <pconcontato@aol.com>
2026-08-07 10:01:46 -0300

    fix: import useEffect in DashboardLayout

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import { Sidebar } from "./Sidebar";
